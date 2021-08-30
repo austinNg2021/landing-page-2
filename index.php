92c7168c6309194fb73f6f151d3a5259363d5d9c
austin <austin.ngyubin@gmail.com>
2021-08-30 12:19:05 +0800

    First release of Second Landing page.

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title></title>
+    </head>
+    <body>
+        <!-- Header Showcase -->
+        <header id="showcase" class="grid">
+            <div class="bg-image"></div>
+            <div class="content-wrap">
+                <h1>Welcome to Acme Web Solutions</h1>
+                <p>Need a flexible recruiting solution as you sell and scale projects?</p>
+                <p>We are here to solve this exact challenge.</p>
+                <a href="#section-b" class="btn">Read More</a>
+            </div>
+        </header>
+
+        <!-- Main Area -->
+        <main id="main">
+            <!-- Section A -->
+            <?php /*
+            <section id="section-a" class="grid">
+                <div class="content-wrap">
+                    <h2 class="content-title">Web & Application Development</h2>
+                    <div class="content-text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde
+                            officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus.
+                            Perferendis officiis deserunt velit voluptas nobis sequi
+                            animi totam, accusantium, ex eius quia, natus quo?</p>
+                    </div>
+                </div>
+            </section>
+            */ ?>
+            <!-- Section B -->
+            
+            <section id="section-b" class="grid">
+                <ul>
+                    <li>
+                        <div class="card">
+                            <img src="1.PNG" alt="">
+                            <div class="card-content">
+                                <h3 class="card-title">Understand Requirements</h3>
+                                <p>Starting with 1 recruiter and scaling from there, collect requirements for job requirements and learn about client's process needs. If the process is not present, create a process suitable to their needs.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <img src="2.png" alt="">
+                            <div class="card-content">
+                                <h3 class="card-title">Start Recruitment</h3>
+                                <p>We take over the process and help the client to schedule, select and onboard candidates, and require intervention only when needed, like for taking interviews. As we fill in positions, we scale down gradually according to the timeline prescribed by the client.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <img src="3.png" alt="">
+                            <div class="card-content">
+                                <h3 class="card-title">Flexible Engagement</h3>
+                                <p>As the client needs changes, we can add or reduce the team and provide pro-rata based support through out.
+                                    We also help consulting the creation of timeline of future projects to the client.</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+            
+            <section id="section-c" class="grid">
+                <div class="content-wrap">
+                    <h2 class="content-title">Our Secret Sauce</h2>
+                    <p>Typically, a customer starts with one recruiter as a test and ramps up as we prove success</p>
+                </div>
+            </section>
+            <section id="section-b" class="grid">
+                <ul>
+                    <li>
+                        <div class="card">
+                            <img src="4.PNG" alt="">
+                            <div class="card-content">
+                                <h3 class="card-title">Understand Requirements</h3>
+                                <p>Offshoring can be harmful to your brand image, which is why we believe in onshoring and nearshoring. Our recruiters are entirely located across the America, which also means the same timezone and workday</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <img src="5.png" alt="">
+                            <div class="card-content">
+                                <h3 class="card-title">Start Recruitment</h3>
+                                <p>Use recruiters who speak and write fluent English, can build relationship with top talent, and are capable of creative sourcing for complex roles</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <img src="6.png" alt="">
+                            <div class="card-content">
+                                <h3 class="card-title">Flexible Engagement</h3>
+                                <p>Recruiting only requires a laptop and a phone, but there is no substitute for good communication skills and grit.</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+
+            <!-- Section C --> <?php /*
+            <section id="section-c" class="grid">
+                <div class="content-wrap">
+                    <h2 class="content-title">We handle all of your digital needs</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nam rerum vel earum error fugiat
+                        cupiditate, dolore eius! Minus, explicabo.</p>
+                </div>
+            </section>
+            */ ?>
+            <!-- Section D -->
+            <section id="section-d" class="grid">
+                <div class="box" style="background-color: #333;">
+                    <?php /* 
+                    <h2 class="content-title">Contact Us</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, suscipit. Rerum ducimus a quod, ut et
+                        voluptas obcaecati unde fuga.</p>
+                    <p>contact@acmewebsolutions.test</p> */ ?>
+                    <img src="6_1.PNG" alt="alt" style="max-width: 100%;"/> 
+                </div>
+                <div class="box">
+                    <h2 class="content-title">Founder's Message</h2>
+                    <p style="text-align: left;">This is the recruitment company I wished existed when I was leading talent for tech startups. Sourcing, engaging, and hiring top talent in tight labor markets is not for the faint of heart</p>
+                    <p style="text-align: left;">John Doe</p>
+                    
+                    <p style="text-align: left;">Founder</p>
+                    <br><br>
+                    <p style="text-align: left;">Awarded Ere.net award for top recruitment department in world</p>
+                </div>
+            </section>
+        </main>
+
+        <!-- Footer -->
+        <footer id="main-footer" class="grid">
+            <div>Acme Web Solutions</div>
+            <div>Project By <a href="http://traversymedia.com" target="_blank">Traversy Media</a></div>
+        </footer>
+
+    </body>
+</html>
